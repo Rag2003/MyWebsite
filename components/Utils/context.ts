5cf6296c33fadb6479604f0d093d68455cac44e8
rguddati <rguddati@iastate.edu>
2025-01-17 20:12:12 -0600

    Updates

--- components/Utils/context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-import PROJECTS_DATA from '../AppData/projects';
-
-const AppContext = createContext({} as any);
-export const ProjectsContext = createContext(PROJECTS_DATA);
-
-export default AppContext;
